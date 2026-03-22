3df6d8451fbcb67d4a1492913a5fe3234beb57f1
Jave Riezzon Palilio <javepalilio@gmail.com>
2026-03-22 11:53:35 +0800

    Fixing CSS styles for Render

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -121,11 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# This tells Django where your CSS/JS files actually live
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# This is where Render will "collect" them for the web
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'homepage' / 'static',
-]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Media files configuration
 MEDIA_URL = '/media/'
